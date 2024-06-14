a47a2cd811d2292c44d3755060a5a112deb5ff45
BFSalvatti <brunofsalvatti@gmail.com>
2024-06-14 19:39:26 +0000

    Apache IAC

---
+++ script-iac2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Atualizando o servidor e instalando recursos ..."
+apt-get apdate -y
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip  -y
+apt-get install wget -y
+echo "Done"
+
+echo "Baixando e alocando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+unzip main.zip
+cd linux-site-dio-main/
+cp -R * /var/www/html/
+echo "Finalizado"
